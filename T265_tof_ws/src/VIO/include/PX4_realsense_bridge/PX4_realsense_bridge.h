d64de9da4edcbbe49a071f82d7b68fc720db9f0e
wjl <2771329872@qq.com>
2024-06-05 14:29:48 +0800

    t265+tof增加组名选择

--- T265_tof_ws/src/VIO/include/PX4_realsense_bridge/PX4_realsense_bridge.h
+++ T265_tof_ws/src/VIO/include/PX4_realsense_bridge/PX4_realsense_bridge.h
@@ -12,46 +12,46 @@
 
 namespace bridge {
 
-  enum class MAV_STATE {
-  MAV_STATE_UNINIT,
-  MAV_STATE_BOOT,
-  MAV_STATE_CALIBRATIN,
-  MAV_STATE_STANDBY,
-  MAV_STATE_ACTIVE,
-  MAV_STATE_CRITICAL,
-  MAV_STATE_EMERGENCY,
-  MAV_STATE_POWEROFF,
-  MAV_STATE_FLIGHT_TERMINATION,
+enum class MAV_STATE {
+    MAV_STATE_UNINIT,
+    MAV_STATE_BOOT,
+    MAV_STATE_CALIBRATIN,
+    MAV_STATE_STANDBY,
+    MAV_STATE_ACTIVE,
+    MAV_STATE_CRITICAL,
+    MAV_STATE_EMERGENCY,
+    MAV_STATE_POWEROFF,
+    MAV_STATE_FLIGHT_TERMINATION,
 };
 
 class PX4_Realsense_Bridge {
- public:
-  PX4_Realsense_Bridge(const ros::NodeHandle& nh);
-  ~PX4_Realsense_Bridge();
+public:
+    PX4_Realsense_Bridge(const ros::NodeHandle& nh,std::string model_name);
+    ~PX4_Realsense_Bridge();
 
-  void publishSystemStatus();
+    void publishSystemStatus();
 
-  std::thread worker_;
+    std::thread worker_;
 
 
- private:
-  ros::NodeHandle nh_;
-  // Subscribers
-  ros::Subscriber odom_sub_;
-  // Publishers
-  ros::Publisher mavros_odom_pub_;
-  ros::Publisher mavros_system_status_pub_;
+private:
+    ros::NodeHandle nh_;
+    // Subscribers
+    ros::Subscriber odom_sub_;
+    // Publishers
+    ros::Publisher mavros_odom_pub_;
+    ros::Publisher mavros_system_status_pub_;
 
-  MAV_STATE system_status_{MAV_STATE::MAV_STATE_UNINIT};
-  MAV_STATE last_system_status_{MAV_STATE::MAV_STATE_UNINIT};
+    MAV_STATE system_status_{MAV_STATE::MAV_STATE_UNINIT};
+    MAV_STATE last_system_status_{MAV_STATE::MAV_STATE_UNINIT};
 
-  std::unique_ptr<std::mutex> status_mutex_;
+    std::unique_ptr<std::mutex> status_mutex_;
 
-  void odomCallback(const nav_msgs::Odometry& msg);
-  bool flag_first_pose_received{false};
-  ros::Time last_callback_time;
+    void odomCallback(const nav_msgs::Odometry& msg);
+    bool flag_first_pose_received{false};
+    ros::Time last_callback_time;
 
-  nav_msgs::Odometry output;
+    nav_msgs::Odometry output;
 
 };
 }
--- T265_tof_ws/src/VIO/src/nodes/px4_realsense_bridge_node.cpp
+++ T265_tof_ws/src/VIO/src/nodes/px4_realsense_bridge_node.cpp
@@ -2,15 +2,26 @@
 #include <sensor_msgs/Range.h>
 #include <mySerial.h>
 using namespace bridge;
+//通过这个函数判断yaml是否加载到参数服务器中
+void Judge_param_load(ros::NodeHandle nh){
+    std::cout<<nh.hasParam("load_param_flag")<<std::endl;
+    while (!nh.hasParam("load_param_flag"))
+  	{
+    	ROS_INFO("等待参数加载...");
+    	ros::Duration(1.0).sleep();
+  	}
+    ROS_INFO("加载成功");
+}
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "PX4_realsense_bridge_node");
-  ros::NodeHandle nh("~");
-  PX4_Realsense_Bridge Bridge(nh);
-  
-  ros::spin();
+    ros::init(argc, argv, "PX4_realsense_bridge_node");
+    ros::NodeHandle nh("~");
+    Judge_param_load(nh);
 
-  Bridge.worker_.join();
-  
-  return 0;
+    std::string modle_name;
+    nh.getParam("modle_name", modle_name);;    
+    PX4_Realsense_Bridge Bridge(nh,modle_name);
+    ros::spin();
+    Bridge.worker_.join();
+    return 0;
 }
