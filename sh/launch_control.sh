51a471a7b9205985acf5e45315ec0f22e240357f
wjl <2771329872@qq.com>
2024-07-22 15:02:15 +0800

    优化脚本文件信息

--- sh/launch_control.sh
+++ sh/launch_control.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #启动mavros,px4,tof和T265
-#这是在修改了固定串口号-tof
-sudo chmod 777 /dev/tof
+#这是在修改了固定串口号-tof和arm_usb
+echo 123 | sudo -S sudo chmod 777 /dev/tof
 sudo chmod 777 /dev/ttyACM0
-#说明,注意修改路径
+
 source /opt/ros/noetic/setup.bash
-source /home/ywb/.bashrc
-uav_workplace_environment_var=/home/wjl/UAV_project/UAV_ws/devel/setup.bash
+source ~/.bashrc
+uav_environment=~/UAV_project/UAV_ws/devel/setup.bash
+t265_tof_environment=~/UAV_project/T265_tof_ws/devel/setup.bash
 gnome-terminal --window -e "bash -c 'roscore; exec bash'" \
---tab -e "bash -c 'sleep 1;source $uav_workplace_environment_var;roslaunch uav uav_yaml.launch; exec bash'" \
---tab -e 'bash -c "sleep 2; roslaunch mavros px4.launch; exec bash"' \
---tab -e 'bash -c "sleep 2; source /home/wjl/UAV_project/T265_tof_ws/devel/setup.bash && roslaunch px4_realsense_bridge bridge.launch; exec bash"' \
---tab -e 'bash -c "sleep 4; rostopic echo /mavros/local_position/pose; exec bash"' \
-# gnome-terminal --window -e 'bash -c "sleep 6; rostopic echo iris_0/mavros/local_position/pose; exec bash"' \  #有组名
+--tab -e "bash -c 'sleep 1;source $uav_environment;roslaunch uav uav_yaml.launch; exec bash'" \
+--tab -e "bash -c 'sleep 2; roslaunch mavros px4.launch; exec bash'" \
+--tab -e "bash -c 'sleep 2; source $t265_tof_environment && roslaunch px4_realsense_bridge bridge.launch; exec bash'" \
+--tab -e "bash -c 'sleep 4; rostopic echo /mavros/local_position/pose; exec bash'" \
--- sh/uav_control.sh
+++ sh/uav_control.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
 #说明,注意修改路径
-workplace_environment_var=/home/wjl/UAV_project/UAV_ws/devel/setup.bash
+workplace_environment_var=~/UAV_project/UAV_ws/devel/setup.bash
 gnome-terminal --window -e "bash -c 'sleep 5;source $workplace_environment_var;rosrun uav uav; exec bash'" \
 --tab -e "bash -c 'sleep 5;source $workplace_environment_var;roslaunch uav uav_Drive.launch; exec bash'" \
