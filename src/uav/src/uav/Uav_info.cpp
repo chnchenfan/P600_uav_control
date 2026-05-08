82e6cf22075721c09a4cbdf78452ac190c4c9fab
Chen Fan <chenfan.chn@outlook.com>
2026-05-08 10:08:20 +0800

    上次修复之后出现解锁后没有形成有效起飞推力，随后触发低油门超时自动 disarm。 进行修复： Uav_info.cpp (line 73)
    先预发送 2 秒合法起飞位置 setpoint。
    
    再解锁。
    
    再切 OFFBOARD。
    
    OFFBOARD 成功后继续强制发送 3 秒起飞高度 setpoint，避免刚解锁阶段低油门 disarm。

--- src/uav/src/uav/Uav_info.cpp
+++ src/uav/src/uav/Uav_info.cpp
@@ -72,23 +72,46 @@ void Uav_info::IsArrived(double x,double y,double z){
 }
 void Uav_info::Set_arm_offboard(){
     ros::Rate loop_rate(30);
-    ROS_INFO("即将进入offboard");
-    while(ros::ok() && current_mavros_state.mode != "OFFBOARD")
+
+    if (setpoint_pos.z < 0.3) {
+        setpoint_pos.z = 0.5;
+    }
+
+    ROS_INFO("预发送起飞位置 setpoint");
+    for (int i = 0; ros::ok() && i < 60; ++i)
     {
-        cmd.move(0, 0,setpoint_pos.z);
-        cmd.offboard();
+        cmd.move(0, 0, setpoint_pos.z);
         loop_rate.sleep();
         ros::spinOnce();
     }
-    ROS_INFO("进入offboard成功");
+
+    ROS_INFO("即将解锁");
     while(ros::ok() && !current_mavros_state.armed)
     {
-        cmd.move(0, 0,setpoint_pos.z);
+        cmd.move(0, 0, setpoint_pos.z);
         cmd.arm();
         loop_rate.sleep();
         ros::spinOnce();
     }
     ROS_INFO("解锁成功");
+
+    ROS_INFO("即将进入offboard");
+    while(ros::ok() && current_mavros_state.mode != "OFFBOARD")
+    {
+        cmd.move(0, 0, setpoint_pos.z);
+        cmd.offboard();
+        loop_rate.sleep();
+        ros::spinOnce();
+    }
+    ROS_INFO("进入offboard成功");
+
+    const ros::Time takeoff_setpoint_until = ros::Time::now() + ros::Duration(3.0);
+    while(ros::ok() && ros::Time::now() < takeoff_setpoint_until)
+    {
+        cmd.move(0, 0, setpoint_pos.z);
+        loop_rate.sleep();
+        ros::spinOnce();
+    }
 }
 void Uav_info::Set_pose(double x,double y,double z){
     // std::cout<<"设置期望的目标点为："<<x<<" "<<y<<" "<<z<<std::endl;
